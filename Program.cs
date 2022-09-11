43158fc0c6b0d1ce0de6b448b7e614c51a889f43
Jakub Zieba <jakub.zie5@gmail.com>
2022-09-11 16:50:12 +0200

    almost done

--- Program.cs
+++ Program.cs
@@ -9,7 +9,90 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-
+            Random roll = new Random();
+            int userWin = 0, aiWin = 0,aiRoll;
+            string[] choices = { "R", "P", "S" };
+            string aiChoice, userChoice;
+            bool valid,play=true;
+            while (play)
+            {
+                aiRoll = roll.Next(choices.Length);
+                aiChoice = choices[aiRoll];
+                do
+                {
+                    Console.Write("WprowadŸ swój wybór ([R]ock / [P]aper / [S]cissors): ");
+                    userChoice = Console.ReadLine();
+                    if (userChoice == "R" || userChoice == "P" || userChoice == "S")
+                    {
+                        Console.WriteLine("Komputer wybra³: " + aiChoice);
+                        if (aiChoice == userChoice)
+                        {
+                            Console.WriteLine("Remis!");
+                        }
+                        else if (userChoice == "R" && aiChoice == "P")
+                        {
+                            Console.WriteLine("Wygrywa komputer!");
+                            aiWin++;
+                        }
+                        else if (userChoice == "R" && aiChoice == "S")
+                        {
+                            Console.WriteLine("Wygrywa gracz!");
+                            userWin++;
+                        }
+                        else if (userChoice == "P" && aiChoice == "R")
+                        {
+                            Console.WriteLine("Wygrywa gracz!");
+                            userWin++;
+                        }
+                        else if (userChoice == "P" && aiChoice == "S")
+                        {
+                            Console.WriteLine("Wygrywa komputer!");
+                            aiWin++;
+                        }
+                        else if (userChoice == "S" && aiChoice == "R")
+                        {
+                            Console.WriteLine("Wygrywa komputer!");
+                            aiWin++;
+                        }
+                        else if (userChoice == "S" && aiChoice == "P")
+                        {
+                            Console.WriteLine("Wygrywa gracz!");
+                            userWin++;
+                        }
+                        Console.WriteLine();
+                        valid = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Niepoprawna figura! Spróbuj ponownie!");
+                        valid = true;
+                    }
+                } while (valid);
+                bool ask=true;
+                while (ask)
+                {
+                    Console.Write("Czy chcesz zagraæ ponownie (Tak/Nie): ");
+                    string doYouPlay;
+                    doYouPlay = Console.ReadLine();
+                    if (doYouPlay == "Tak")
+                    {
+                        play = true;
+                        ask = false;
+                    }
+                    else if (doYouPlay == "Nie")
+                    {
+                        play = false;
+                        ask = false;
+                        Console.WriteLine("Koñczymy rozgrywkê!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Niepoprawdna odpowiedŸ! Spróbuj jeszcze raz!");
+                    }
+                }
+            }
+            Console.WriteLine("\nKomputer wygra³ " + aiWin + " razy || Gracz wygra³ " + userWin + " razy. Dziêkujemy za rozgrywkê :)");
+            Console.ReadLine();
         }
     }
 }
